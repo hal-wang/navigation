4c7c947bf3d42a14efd39c7c45830d8179e1fb79
hal-wang <hi@hal.wang>
2022-08-26 19:48:54 +0800

    chore: update deps

--- src/startup.ts
+++ src/startup.ts
@@ -1,6 +1,8 @@
-import { Startup } from "@ipare/core";
 import "@ipare/inject";
 import "@ipare/env";
+import "@ipare/logger";
+
+import { Startup } from "@ipare/core";
 import { CollectionService } from "./services/collection.service";
 import { CbappService } from "./services/cbapp.service";
 import { InjectType } from "@ipare/inject";
@@ -13,5 +15,12 @@ export default function <T extends Startup>(startup: T, mode: string): T {
     .useInject()
     .inject(CollectionService, InjectType.Singleton)
     .inject(CbappService, InjectType.Singleton)
+    .useConsoleLogger()
+    .use(async (ctx, next) => {
+      const logger = await ctx.getLogger();
+      logger.info("event: " + JSON.stringify(ctx.lambdaEvent));
+      logger.info("context: " + JSON.stringify(ctx.lambdaContext));
+      await next();
+    })
     .add(NavigatorMiddleware);
 }
